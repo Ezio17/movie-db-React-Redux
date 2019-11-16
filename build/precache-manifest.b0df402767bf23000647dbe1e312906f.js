3f441109b2dd10e22953f38ccac3f4a30ab7cdd0
ezio17 <xd_ezio@ukr.net>
2019-11-16 18:12:57 +0200

    build was changed

---
+++ build/precache-manifest.b0df402767bf23000647dbe1e312906f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec39e80857ec986bbec2d7e4807da4a4",
+    "url": "/movie-db-React-Redux/build/index.html"
+  },
+  {
+    "revision": "efa530ce6af647fc32fa",
+    "url": "/movie-db-React-Redux/build/static/css/2.fa05ffb1.chunk.css"
+  },
+  {
+    "revision": "d75724aa48bd9e9e962b",
+    "url": "/movie-db-React-Redux/build/static/css/main.4676ef9a.chunk.css"
+  },
+  {
+    "revision": "efa530ce6af647fc32fa",
+    "url": "/movie-db-React-Redux/build/static/js/2.c92cb2f2.chunk.js"
+  },
+  {
+    "revision": "d75724aa48bd9e9e962b",
+    "url": "/movie-db-React-Redux/build/static/js/main.b677b96c.chunk.js"
+  },
+  {
+    "revision": "bebd10237125423eb106",
+    "url": "/movie-db-React-Redux/build/static/js/runtime-main.e48fd3e2.js"
+  },
+  {
+    "revision": "945e9186c80e64c38796eb650f95f69c",
+    "url": "/movie-db-React-Redux/build/static/media/iconfinder_23.Videos_290127.945e9186.svg"
+  },
+  {
+    "revision": "2086744a63590aa5f9f4c347c7dda66f",
+    "url": "/movie-db-React-Redux/build/static/media/iconfinder_icon-111-search_314478.2086744a.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/movie-db-React-Redux/build/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/movie-db-React-Redux/build/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/movie-db-React-Redux/build/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/movie-db-React-Redux/build/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0681597ee714ecd6e05ccc3d4dfd7b34.js"
+  "/movie-db-React-Redux/build/precache-manifest.b0df402767bf23000647dbe1e312906f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/movie-db-React-Redux/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
